998b0e964670716148e1dc8ee05d6b83b75e1615
RomanVasylkovskyi <101647171+RomanVasylkovskyi@users.noreply.github.com>
2023-10-17 22:31:27 +0300

    Finish

---
+++ AT-211-v23-lab4/AT-211-v23-lab4/ComplexNumber.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace AT_211_v23_lab4
+{
+    // A class to represent complex numbers in algebraic form a = x + yi
+    public class ComplexNumber
+    {
+        // The real part of the complex number
+        public double Real { get; set; }
+
+        // The imaginary part of the complex number
+        public double Imaginary { get; set; }
+
+        // Constructor to initialize the complex number
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        // Override the ToString method for convenient complex number output
+        public override string ToString()
+        {
+            if (Imaginary >= 0)
+            {
+                return $"{Real} + {Imaginary}i";
+            }
+            else
+            {
+                return $"{Real} - {Math.Abs(Imaginary)}i";
+            }
+        }
+
+        // Addition of two complex numbers
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            double realSum = a.Real + b.Real;
+            double imaginarySum = a.Imaginary + b.Imaginary;
+            return new ComplexNumber(realSum, imaginarySum);
+        }
+
+        // Subtraction of two complex numbers
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            double realDiff = a.Real - b.Real;
+            double imaginaryDiff = a.Imaginary - b.Imaginary;
+            return new ComplexNumber(realDiff, imaginaryDiff);
+        }
+
+        // Multiplication of two complex numbers
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            double realProduct = (a.Real * b.Real) - (a.Imaginary * b.Imaginary);
+            double imaginaryProduct = (a.Real * b.Imaginary) + (a.Imaginary * b.Real);
+            return new ComplexNumber(realProduct, imaginaryProduct);
+        }
+
+        // Division of two complex numbers
+        public static ComplexNumber operator /(ComplexNumber left, ComplexNumber right)
+        {
+            if (left.Imaginary != 0 && right.Imaginary != 0)
+            {
+                double denominator = right.Real * right.Real + right.Imaginary * right.Imaginary;
+                double realResult = (left.Real * right.Real + left.Imaginary * right.Imaginary) / denominator;
+                double imaginaryResult = (left.Imaginary * right.Real - left.Real * right.Imaginary) / denominator;
+                return new ComplexNumber(realResult, imaginaryResult);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Cannot be divided by zero");
+            }
+        }
+
+        // Check conjugation of ComplexNumber1 and ComplexNumber2
+        public bool AreConjugates(ComplexNumber other)
+        {
+            return this.Real == other.Real && this.Imaginary == -other.Imaginary;
+        }
+    }
+}
---
+++ AT-211-v23-lab4/AT-211-v23-lab4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AT_211_v23_lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a random number generator
+            Random r = new Random();
+
+            // Define the range for random numbers
+            const int maxRandom = 10;
+            const int minRandom = -10;
+
+            // Create four complex numbers (a, b, c, d) with random real and imaginary parts
+            ComplexNumber a = new ComplexNumber(r.Next(minRandom, maxRandom), r.Next(minRandom, maxRandom));
+            ComplexNumber b = new ComplexNumber(r.Next(minRandom, maxRandom), r.Next(minRandom, maxRandom));
+            ComplexNumber c = new ComplexNumber(r.Next(minRandom, maxRandom), r.Next(minRandom, maxRandom));
+            ComplexNumber d = new ComplexNumber(r.Next(minRandom, maxRandom), r.Next(minRandom, maxRandom));
+
+            // Perform complex number operations to compute R1 and R2
+            ComplexNumber R1 = a - ((b + c) / a);
+            ComplexNumber R2 = d * ((a + c) / a);
+
+            // Display the initial complex numbers (a, b, c, d)
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"c = {c}");
+            Console.WriteLine($"d = {d}");
+
+            Console.WriteLine("========================");
+
+            // Display the results R1 and R2
+            Console.WriteLine($"R1 = {R1}");
+            Console.WriteLine($"R2 = {R2}");
+
+            // Check conjugation of R1 and R2
+            bool areConjugates = R1.AreConjugates(R2);
+            if (areConjugates)
+            {
+                Console.WriteLine("R1 and R2 are conjugates.");
+            }
+            else
+            {
+                Console.WriteLine("R1 and R2 are not conjugates.");
+            }
+        }
+    }
+}
